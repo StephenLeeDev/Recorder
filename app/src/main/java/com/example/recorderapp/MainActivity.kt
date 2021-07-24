f71126d47a2df95cb361a765631ce19e9c94a391
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-07-24 23:05:52 +0900

    ADD : Add Request Permission Method

--- app/src/main/java/com/example/recorderapp/MainActivity.kt
+++ app/src/main/java/com/example/recorderapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recorderapp
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,16 +10,31 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.recordButton)
     }
 
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
     private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
         initViews()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
