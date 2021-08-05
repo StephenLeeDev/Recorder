f15672bc827e225b7146d3e90fda0fd240ef96ce
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-08-05 20:54:13 +0900

    EDIT : Building SoundVisualizerView For Recording UI IV

--- app/src/main/java/com/example/recorderapp/MainActivity.kt
+++ app/src/main/java/com/example/recorderapp/MainActivity.kt
@@ -10,6 +10,10 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private val soundVisualizerView: SoundVisualizerView by lazy {
+        findViewById(R.id.soundVisualizerView)
+    }
+
     private val resetButton: Button by lazy {
         findViewById(R.id.resetButton)
     }
@@ -108,6 +112,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun bindViews() {
+        soundVisualizerView.onRequestCurrentAmplitude = {
+            recorder?.maxAmplitude ?: 0
+        }
         recordButton.setOnClickListener {
             when (state) {
                 State.BEFORE_RECORDING -> {
--- app/src/main/java/com/example/recorderapp/SoundVisualizerView.kt
+++ app/src/main/java/com/example/recorderapp/SoundVisualizerView.kt
@@ -9,15 +9,26 @@ import kotlin.random.Random
 
 class SoundVisualizerView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
 
-    val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    var onRequestCurrentAmplitude: (() -> Int)? = null
+
+    private val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = context.getColor(R.color.purple_500)
         strokeWidth = LINE_WIDTH
         strokeCap = Paint.Cap.ROUND
     }
-    var drawingWidth: Int = 0
-    var drawingHeight: Int = 0
+    private var drawingWidth: Int = 0
+    private var drawingHeight: Int = 0
 //    var drawingAmplitudes: List<Int> = emptyList()
-    var drawingAmplitudes: List<Int> = (0..10).map { Random.nextInt(Short.MAX_VALUE.toInt())}
+    private var drawingAmplitudes: List<Int> = (0..10).map { Random.nextInt(Short.MAX_VALUE.toInt())}
+
+    private val visualizeRepeatAction: Runnable = object : Runnable {
+        override fun run() {
+            val currentAmplitude = onRequestCurrentAmplitude?.invoke() ?: 0
+            drawingAmplitudes = listOf(currentAmplitude) + drawingAmplitudes
+
+            handler?.postDelayed(this, ACTION_INTERVAL)
+        }
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -53,5 +64,6 @@ class SoundVisualizerView(context: Context, attrs: AttributeSet? = null) : View(
         private const val LINE_WIDTH = 10F
         private const val LINE_SPACE = 15F
         private const val MAX_AMPLITUDE = Short.MAX_VALUE.toFloat()
+        private const val ACTION_INTERVAL = 20L
     }
 }
